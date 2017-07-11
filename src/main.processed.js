2725104e0356de35a921cb450ea736cf53f314ad
javarome <javarome@gmail.com>
2017-07-11 13:41:00 +0200

    Removed processed files

--- src/main.processed.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-//main entry point
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var Prosper_module_ts_1 = require("./Prosper.module.ts");
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(Prosper_module_ts_1.ProsperModule);
