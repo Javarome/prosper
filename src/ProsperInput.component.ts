b5edebd4e92a3d245e52a994d21a309a84060df3
javarome <javarome@gmail.com>
2017-08-06 16:13:15 +0200

    Added manual iteration mode

--- src/ProsperInput.component.ts
+++ src/ProsperInput.component.ts
@@ -1,7 +1,7 @@
 import {Component, ElementRef, Input} from "@angular/core";
 
 import {Prosper} from "./Prosper.component.ts";
-import {NodeFactory} from "./ProsperGraph.component.ts";
+import {NodeFactory, ProsperMemoryNode} from "./ProsperGraph.component.ts";
 
 class DefaultNodeFactory implements NodeFactory {
   create(value: any): ProsperMemoryNode {
@@ -14,7 +14,6 @@ class DefaultNodeFactory implements NodeFactory {
     };
   }
 }
-
 class CharNodeFactory extends DefaultNodeFactory {
   merge(node1: ProsperMemoryNode, node2: ProsperMemoryNode) {
     const conceptNode = this.create(node1.label + node2.label);
@@ -26,10 +25,44 @@ class CharNodeFactory extends DefaultNodeFactory {
 
 class WordNodeFactory extends DefaultNodeFactory {
   merge(node1: ProsperMemoryNode, node2: ProsperMemoryNode) {
-    const conceptNode = this.create(node1.label + ' ' + node2.label);
+    const conceptNode: ProsperMemoryNode = this.create(node1.label + ' ' + node2.label);
     conceptNode.color = 'green';
     conceptNode.concept = true;
     return conceptNode;
+  }
+}
+
+class AutoIterator<T> {
+  constructor(prosper: Prosper, nodeFactory) {
+    this.prosper = prosper;
+    this.nodeFactory = nodeFactory;
+  }
+
+  iterate(sampling: <T>[], params) {
+    let i = 0;
+    sampling.forEach(node => {
+      setTimeout(() => this.prosper.input(node, this.nodeFactory), params.speed * i++);
+    });
+  }
+}
+
+class ManualIterator<T> {
+  constructor(prosper: Prosper, nodeFactory) {
+    this.prosper = prosper;
+    this.nodeFactory = nodeFactory;
+  }
+
+  iterate(sampling: <T>[]) {
+    this.sampling = sampling;
+    this.i = 0;
+    this.next();
+  }
+
+  next() {
+    this.prosper.input(this.sampling[this.i], this.nodeFactory);
+    this.i++;
+    this.hasNext = this.i < this.sampling.length;
+    console.log('next is ', this.sampling[this.i])
   }
 }
 
@@ -39,31 +72,55 @@ class WordNodeFactory extends DefaultNodeFactory {
 })
 export class ProsperInput {
   private value;
+
   private speed: number;
   private sampleCharsType;
   private sampleWordsType;
-  private items;
+  iteration;
   private sampleType;
+  private sampleTypes;
+  private autoIteration;
+  private iterationTypes;
+  private iterationType;
+  private autoIteration;
+  private manualIteration;
 
   @Input() private prosper: Prosper;
-  
+
   constructor(private $element: ElementRef) {
     this.value = "";
     this.speed = 0;
     this.sampleCharsType = {
       value: "chars",
       label: "Chars",
-      sample: input => input.split(""),
+      sample: (input, nodeFactory) => input.split("").map(sample => nodeFactory.create(sample))
       nodeFactory: new CharNodeFactory()
     };
     this.sampleWordsType = {
       value: "words",
       label: "Words",
-      sample: input => input.split(" "),
+      sample: (input, nodeFactory) => input.split(" ").map(sample => nodeFactory.create(sample)),
       nodeFactory: new WordNodeFactory()
     };
-    this.items = [this.sampleCharsType, this.sampleWordsType];
+    this.sampleTypes = [this.sampleCharsType, this.sampleWordsType];
     this.sampleType = this.sampleCharsType;
+
+    this.autoIteration = {
+      value: 'auto',
+      label: 'Automatic',
+      create(prosper: Prosper, nodeFactory) {
+        return new AutoIterator<string>(prosper, nodeFactory);
+      }
+    };
+    this.manualIteration = {
+      value: 'manual',
+      label: 'Manual',
+      create(prosper: Prosper, nodeFactory) {
+        return new ManualIterator<string>(prosper, nodeFactory);
+      }
+    };
+    this.iterationTypes = [this.autoIteration, this.manualIteration];
+    this.iterationType = this.autoIteration;
   }
 
   log(msg) {
@@ -101,14 +158,11 @@ export class ProsperInput {
   }
 
   submit() {
-    const sampling = this.sampleType.sample(this.value);
     const nodeFactory = this.sampleType.nodeFactory;
+    const sampling = this.sampleType.sample(this.value, nodeFactory);
     // this.prosper.input(Date.now().toString());
-    let i = 0;
-    sampling.forEach(sample => {
-      const newNode = nodeFactory.create(sample);
-      setTimeout(() => this.prosper.input(newNode, nodeFactory), this.speed * i++);
-    });
+    this.iteration = this.iterationType.create(this.prosper, nodeFactory);
+    this.iteration.iterate(sampling, this);
     this.value = "";
     this.$element.nativeElement.querySelector("input").focus();
   }
